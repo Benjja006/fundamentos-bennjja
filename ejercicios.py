e8cd5916f4faa029dcffddfa79072a40a4fc4f01
Benjja006 <sepulvedabenjamin464@gmail.com>
2025-04-14 13:32:28 -0400

    commit inicial

---
+++ ejercicios.py
@@ -0,0 +1,9 @@
+print("Benjamin Sepúlveda")
+print("18")
+
+print("Los diablotes - Juliano Sosa")
+
+print(":)")
+print(":(")
+print(":v")
+
---
+++ inicio.py
@@ -0,0 +1,10 @@
+# Este es mi primer programa en Python
+print("¡Hola mundo!")
+print("Bienvenidos al taller de Python")
+# Podemos imprimir números directamente
+print(2023)
+# O hacer operaciones básicas
+print(20 + 25)
+print(True)
+print(False)
+print(4 != 3)
